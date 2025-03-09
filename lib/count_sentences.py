822516df1dcb3d6714ac1ef666a641e2561292dc
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2025-03-09 16:17:06 +0300

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value  # Uses the setter to validate input
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")  # Print instead of raising an error
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        sentences = re.split(r"[.!?]+", self.value.strip())
+        return len([s for s in sentences if s.strip()])
+
+# Test case simulation
+string = MyString()
+string.value = 123  # Should print: "The value must be a string."
